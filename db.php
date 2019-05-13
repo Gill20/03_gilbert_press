773b1afa68e24158153eb98df1ea3b63f4fcf49a
gill20 <ohgilbert345@gmail.com>
2019-05-13 13:13:38 +0800

    first commit

---
+++ db.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php // define database connection variables as constant
+
+define('DB_USER',"root");//db user
+
+define('DB_PASSWORD',"");// db password
+
+define('DB_DATABASE', "03_gilbert_pessdb");// database name
+
+define('DB_SERVER', "localhost");// db server
+
+?>
+
+</body>
+</html>
